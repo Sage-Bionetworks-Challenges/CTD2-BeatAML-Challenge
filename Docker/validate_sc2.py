9ece0d670831e0f6f2ed0824451c2a0c33de775e
verena <vpchung@users.noreply.github.com>
2019-11-20 11:16:41 -0800

    Added lab_id check

--- Docker/validate_sc2.py
+++ Docker/validate_sc2.py
@@ -7,8 +7,13 @@ import json
 
 import pandas as pd
 
+SC2_DTYPE = {
+    'lab_id': str,
+    'responder': int
+}
 
-def main(submission, entity_type, results="results.json"):
+
+def main(submission, goldstandard, results):
     """Validate submission and write results to JSON.
 
     Args:
@@ -19,24 +24,38 @@ def main(submission, entity_type, results="results.json"):
 
     invalid_reasons = []
 
-    if not submission.endswith(".csv"):
+    truth = pd.read_csv(goldstandard, dtype=SC2_DTYPE)
+    try:
+        predictions = pd.read_csv(submission, dtype=SC2_DTYPE)
+    except Exception:
         invalid_reasons.append(
             "Prediction file must be a two-column CSV file.")
+
     else:
-        predictions = pd.read_csv(submission, sep=",")
 
-        header = list(predictions.columns)
-        try:
-            assert header == ["lab_id", "responder"]
-        except AssertionError:
-            invalid_reasons.append("Column names must be: lab_id,responder.")
-
-        responses = set(predictions.responder)
-        try:
-            assert responses == {0, 1}
-        except AssertionError:
+        # Column names are [lab_id, responder]
+        header = predictions.columns.tolist()
+        expected_cols = ["lab_id", "responder"]
+        if header != expected_cols:
             invalid_reasons.append(
-                " Responder values must be a Boolean (0 or 1).")
+                f"Invalid columns names: {header}. Expected: {expected_cols}")
+
+        else:
+
+            # All expected lab_ids are accounted for
+            pred_lab_ids = predictions.lab_id.tolist()
+            gs_lab_ids = truth.lab_id.tolist()
+            all_ids_found = all(
+                lab_id in pred_lab_ids for lab_id in gs_lab_ids)
+            if not all_ids_found:
+                invalid_reasons.append(
+                    "One or more expected lab_id(s) missing")
+
+            # Responder values are Boolean (0, 1)
+            responders = set(predictions.responder)
+            if responders != {0, 1}:
+                invalid_reasons.append(
+                    "'responder' values should be a Boolean (0 or 1)")
 
     prediction_file_status = "INVALID" if invalid_reasons else "VALIDATED"
 
@@ -52,10 +71,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--submission_file",
                         required=True, help="Submission file")
-    parser.add_argument("-e", "--entity_type",
-                        required=True, help="Synapse entity type")
+    parser.add_argument("-g", "--goldstandard",
+                        required=True, help="Truth file")
     parser.add_argument("-r", "--results",
                         required=True, help="Results file")
 
     args = parser.parse_args()
-    main(args.submission_file)
+    main(args.submission_file, args.goldstandard, args.results)
