5a0d89029134a5582ddc815d178c1ad02b7b71cb
verena <vpchung@users.noreply.github.com>
2020-08-19 14:39:41 -0700

    generate matrices of correlations and p-values; export as RDS

--- analysis/drug_correlations.R
+++ analysis/drug_correlations.R
@@ -1,6 +1,7 @@
 suppressPackageStartupMessages(library(tidyverse))
-library(reticulate)
-library(pheatmap)
+suppressPackageStartupMessages(library(pacman))
+p_load(reticulate)
+p_load(pheatmap)
 
 
 # `synasper` not working on local computer, so using `reticulate` to
@@ -35,9 +36,11 @@ drug.mat <- aucs %>%
 pheatmap(
   cor(drug.mat, method = "pearson", use = "complete.obs"), 
   main = "Drug vs. Drug Correlations",
-  fontsize = 8,
+  fontsize = 5, 
   clustering_distance_rows = "correlation", 
-  clustering_distance_cols = "correlation"
+  clustering_distance_cols = "correlation",
+  file = "drug_v_drug.png",
+  width = 10, height = 10
 )
 
 
@@ -66,39 +69,55 @@ hist(rnaseq[,2])
 
 # For every gene, find the mean and standard deviation, then perform a 
 # smoothScatter to help filter the data
-mu <- apply(rnaseq, 1, mean)
-SD <- apply(rnaseq, 1, sd)
-smoothScatter(mu, SD)
-filtered.rnaseq <- rnaseq[mu > 5 & SD > 3.5,] %>%
-  rownames_to_column("Gene") %>%
-  left_join(gene_symbols, by = "Gene") %>%
-  select(-Gene) %>%
-  column_to_rownames("Symbol")
+# mu <- apply(rnaseq, 1, mean)
+# SD <- apply(rnaseq, 1, sd)
+# smoothScatter(mu, SD)
+# filtered.rnaseq <- rnaseq[mu > 5 & SD > 3.5,] %>%
+#   rownames_to_column("Gene") %>%
+#   left_join(gene_symbols, by = "Gene") %>%
+#   select(-Gene) %>%
+#   column_to_rownames("Symbol")
   
 
 # Plot the Pearson correlations as a heatmap
-pheatmap(
-  cor(t(filtered.rnaseq), drug.mat, method = "pearson", use = "complete.obs"),
-  main = "Gene vs. Drug Correlations",
-  fontsize = 8,
-  clustering_distance_rows = "correlation", 
-  clustering_distance_cols = "correlation"
-)
+# pheatmap(
+#   cor(t(filtered.rnaseq), drug.mat, method = "pearson", use = "complete.obs"),
+#   main = "Gene vs. Drug Correlations",
+#   fontsize = 8,
+#   clustering_distance_rows = "correlation", 
+#   clustering_distance_cols = "correlation"
+# )
 
 
-# Compute the matrix of correlation p-values
-#   (source: http://www.sthda.com/upload/rquery_cormat.r)
-cor.pmat <- function(x, ...) {
-  mat <- as.matrix(x)
-  n <- ncol(mat)
-  p.mat<- matrix(NA, n, n)
-  diag(p.mat) <- 0
-  for (i in 1:(n - 1)) {
-    for (j in (i + 1):n) {
-      tmp <- cor.test(mat[, i], mat[, j], ...)
-      p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+# Compute the matrix of correlations and p-values
+#   (reference: http://www.sthda.com/upload/rquery_cormat.r)
+cor.mats <- function(mat1, mat2) {
+  mat1 <- as.matrix(mat1)
+  mat2 <- as.matrix(mat2)
+  n1 <- ncol(mat1)
+  n2 <- ncol(mat2)
+  p.mat <- c.mat <- matrix(NA, n1, n2)
+  for (i in 1:n1) {
+    for (j in 1:n2) {
+      tmp <- cor.test(mat1[, i], mat2[, j], method = "pearson", use = "complete.obs")
+      p.mat[i, j] <- tmp$p.value
+      c.mat[i, j] <- tmp$estimate
     }
   }
-  colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
-  p.mat
+  rownames(p.mat) <- rownames(c.mat) <- colnames(mat1)
+  colnames(p.mat) <- colnames(c.mat) <- colnames(mat2)
+  list(r = c.mat, p = p.mat)
 }
+
+# corrs <- cor.mats(drug.mat, t(rnaseq))
+# p.mat <- corrs$p
+# c.mat <- corrs$r
+# 
+# Export matrices to RDS for subsequent faster loading times.
+# saveRDS(p.mat, file = "p_values.rds")
+# saveRDS(c.mat, file = "correlations.rds")
+
+# Load RDS for correlation and p-value matrices
+p.mat <- readRDS("p_values.rds")
+c.mat <- readRDS("correlations.rds")
+
